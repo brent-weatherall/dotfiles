ae8fceabb4d788b7226242dad056a2e965f16e83
Brent Weatherall <weatherall@gmail.com>
2022-06-28 12:26:48 -0400

    updating display config to stop screen tearing

--- dot_config/screenlayout/executable_default.sh
+++ dot_config/screenlayout/executable_default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --mode 2560x1080 --pos 0x0 --rotate normal --output DisplayPort-1 --primary --mode 2560x1080 --pos 0x1080 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --off
+xrandr --output DisplayPort-0 --rate 74.99 --set "TearFree" on --mode 2560x1080 --pos 0x0 --rotate normal --output DisplayPort-1 --rate 74.99 --set "TearFree" on --primary --mode 2560x1080 --pos 0x1080 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --off
